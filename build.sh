f855c8461397b7a602cfa29fe797b9c8d215ca10
Muh Muhten <muh.muhten@gmail.com>
2016-03-08 22:07:47 -0500

    parameterize build.sh to build other versions, default by branch

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-docker run --rm vaca/builder -p alpine-keys,apk-tools-static | xz > rootfs.tar.xz
+docker run --rm vaca/builder \
+	-r "${1-`git symbolic-ref --short HEAD`}" \
+	-p alpine-keys,apk-tools-static \
+	| xz > rootfs.tar.xz
